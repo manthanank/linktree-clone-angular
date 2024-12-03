b72fdb19d1f00f9dc0912d601ce6d45afed59beb
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-12-03 11:35:25 +0530

    feat: update configuration and remove unused files; add new SVG icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,33 @@
+import { NgOptimizedImage } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Router, RouterOutlet } from '@angular/router';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    NgOptimizedImage,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
+  imports: [NgOptimizedImage],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  title = 'linktree-clone-angular';
-
   data: any;
   name: any;
   href: any;
-  // public href: string = "";
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.http.get('/assets/data.json').subscribe(data => {
+    this.http.get('data.json').subscribe((data) => {
       this.data = data;
       this.name = data;
-      // console.log(this.data);
     });
   }
 
   shareUrl() {
     if (navigator.share) {
       navigator.share({
-        title: 'My Angular App',
+        title: 'My Awesome App',
         text: 'Check out this awesome Angular app',
-        url: window.location.href
+        url: window.location.href,
       });
     }
   }
@@ -52,7 +38,7 @@ export class AppComponent implements OnInit {
       navigator.share({
         title: 'Site Link',
         text: 'Check out this link',
-        url: url
+        url: url,
       });
     }
   }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+  ],
 };
